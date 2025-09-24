303e89785bc750ea464f6eff4375d8bc53b6b32d
pm <pm@101media.ru>
2025-09-24 22:00:26 +0300

    fix resources

--- src/AssetsServiceProvider.php
+++ src/AssetsServiceProvider.php
@@ -8,13 +8,9 @@ class AssetsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'fortify');
-        $this->loadTranslationsFrom(__DIR__.'/../lang', 'fortify');
-
         $this->publishes([
             __DIR__.'/../lang' => $this->app->langPath(),
             __DIR__.'/../resources/views' => resource_path('views/auth'),
-        ], 'fortify');
-
+        ]);
     }
 }
